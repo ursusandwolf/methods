a0a220e1561d44d327ccffea51b45c34ab323e2b
ursusandwolf <129175187+ursusandwolf@users.noreply.github.com>
2023-03-29 15:06:15 +0300

    Create update.py

---
+++ update.py
@@ -0,0 +1,11 @@
+my_set = {'government', 'control', 'winter', 'few', 'generation',
+          'service', 'national', 'tradition', 'government'}
+data = ["concert", "brown", "jacket", "value"]
+
+print(*my_set)
+print(len(my_set))
+
+my_set.update(data)
+
+print(*my_set)
+print(len(my_set))
